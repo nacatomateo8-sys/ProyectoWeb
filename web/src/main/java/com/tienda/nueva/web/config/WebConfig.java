dc8b13028b4ba38946ed0ba536c25352b58c0f08
nacatomateo8-sys <nacatomateo8@gmail.com>
2026-06-02 19:22:25 -0500

    Configurar servidor estático para imágenes y recursos

---
+++ web/src/main/java/com/tienda/nueva/web/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.tienda.nueva.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Sirve archivos estáticos desde /img/, /css/, /js/, etc.
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("classpath:/static/img/");
+        
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+        
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+    }
+}
